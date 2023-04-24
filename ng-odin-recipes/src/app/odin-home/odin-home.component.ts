2cf6823e5df922951a4748efbbf8653363ec20f5
Anupam Vamsi <anupam.ismav@gmail.com>
2023-04-24 13:39:15 +0530

    1. Add Odin Recipes (with logo) component - add html, css, 2. Add app-odin-home in app.component.html, 3. Need to refactor .spec.ts file though

---
+++ ng-odin-recipes/src/app/odin-home/odin-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-odin-home',
+  templateUrl: './odin-home.component.html',
+  styleUrls: ['./odin-home.component.css'],
+})
+export class OdinHomeComponent {}
