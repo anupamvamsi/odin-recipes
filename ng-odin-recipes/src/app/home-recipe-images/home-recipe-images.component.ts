f2d0b834c22cd3790a105644c786d5cdefd7afc5
Anupam Vamsi <anupam.ismav@gmail.com>
2023-04-24 17:11:23 +0530

    1. Last implementation to add alt text along with the src image with two arrays in *ngFor did not work, fixed it now by using a 2D array with src and alts in it

--- ng-odin-recipes/src/app/home-recipe-images/home-recipe-images.component.ts
+++ ng-odin-recipes/src/app/home-recipe-images/home-recipe-images.component.ts
@@ -6,25 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-recipe-images.component.scss'],
 })
 export class HomeRecipeImagesComponent {
-  srcImages: string[];
-  altTexts: string[];
+  srcAlts: string[][];
 
   constructor() {
-    this.srcImages = [];
-    this.altTexts = [];
+    this.srcAlts = [];
   }
 
   ngOnInit() {
-    this.srcImages.push(
-      'https://images.unsplash.com/photo-1525351484163-752941444d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-      'https://images.unsplash.com/photo-1603033172872-c252511c7b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-      'https://images.unsplash.com/photo-1551014029-03820d0b885?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80'
-    );
-
-    this.altTexts.push(
-      'Link to Eggs and Bread Recipe',
-      'Link to Maggi and Eggs Recipe',
-      'Link to Boiled Eggs Recipe'
+    this.srcAlts.push(
+      [
+        'https://images.unsplash.com/photo-1525351484163-7529414344d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+        'Link to Eggs and Bread Recipe',
+      ],
+      [
+        'https://images.unsplash.com/photo-1603033172872-c2525115c7b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+        'Link to Maggi and Eggs Recipe',
+      ],
+      [
+        'https://images.unsplash.com/photo-1551014029-03820d0b88f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+        'Link to Boiled Eggs Recipe',
+      ]
     );
   }
 }
