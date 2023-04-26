12b472eb6346574fbbf200b3e2f260dd0540871d
Anupam Vamsi <anupam.ismav@gmail.com>
2023-04-26 17:24:33 +0530

    1. Add json-server to act as a pseudo-db, 2. Update app-recipe-images' .ts file

--- ng-odin-recipes/src/app/home-recipe-images/home-recipe-images.component.ts
+++ ng-odin-recipes/src/app/home-recipe-images/home-recipe-images.component.ts
@@ -21,25 +21,5 @@ export class HomeRecipeImagesComponent {
 
   ngOnInit() {
     this.recipes.push(new BreadAndEggs(), new MaggiAndEggs(), BoiledEggs);
-    // this.imageLinks.push(
-    //   {
-    //     srcImage:
-    //       'https://images.unsplash.com/photo-1525351484163-7529414344d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-    //     altText: 'Link to Eggs and Bread Recipe',
-    //     linkToPage: '/recipe/bread-and-eggs',
-    //   },
-    //   {
-    //     srcImage:
-    //       'https://images.unsplash.com/photo-1603033172872-c2525115c7b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-    //     altText: 'Link to Maggi and Eggs Recipe',
-    //     linkToPage: '/recipe/maggi-and-eggs',
-    //   },
-    //   {
-    //     srcImage:
-    //       'https://images.unsplash.com/photo-1551014029-03820d0b88f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
-    //     altText: 'Link to Boiled Eggs Recipe',
-    //     linkToPage: '/recipe/boiled-eggs',
-    //   }
-    // );
   }
 }
